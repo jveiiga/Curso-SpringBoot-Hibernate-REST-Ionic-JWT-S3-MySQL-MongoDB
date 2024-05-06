b3e38101b22ebfe827f879eb14a946c9b3eb0cb5
jveiiga <jeferson.veiiga@icloud.com>
2024-05-06 13:34:31 -0300

    Enviando subTotal e Total do pedido

--- cursomc/src/main/java/com/example/cursomc/domain/ItemPedido.java
+++ cursomc/src/main/java/com/example/cursomc/domain/ItemPedido.java
@@ -20,6 +20,7 @@ public class ItemPedido implements Serializable {
     private  Integer quantidade;
     private Double preco;
 
+
     public ItemPedido() {
     }
 
@@ -29,6 +30,10 @@ public class ItemPedido implements Serializable {
         this.desconto = desconto;
         this.quantidade = quantidade;
         this.preco = preco;
+    }
+
+    public double getSubtotal() {
+        return (preco - desconto) * quantidade;
     }
 
     @JsonIgnore
