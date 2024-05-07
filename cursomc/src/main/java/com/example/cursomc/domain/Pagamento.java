dded5ff033e45d42e33d7ed16e8e1f55bfc999ad
jveiiga <jeferson.veiiga@icloud.com>
2024-05-06 22:32:03 -0300

    Enviando inserção de pedido status 415, error Unsupported Media Type

--- cursomc/src/main/java/com/example/cursomc/domain/Pagamento.java
+++ cursomc/src/main/java/com/example/cursomc/domain/Pagamento.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import com.example.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -16,6 +17,7 @@ import jakarta.persistence.OneToOne;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="@type")
 public abstract class Pagamento implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- cursomc/src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
+++ cursomc/src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
@@ -2,11 +2,15 @@ package com.example.cursomc.domain;
 
 import com.example.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import jakarta.persistence.Entity;
 
 import java.util.Date;
 
+
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
 
     private static final long serialVersionUID = 1L;
